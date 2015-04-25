ab78d09a25abce9fe9f29d7282b3b8993832e620
Nikolaibibo <nbockholt@fork.de>
2015-04-25 18:51:17 +0200

    added pushbullet notification on error, untested!

--- twitter.js
+++ twitter.js
@@ -1,4 +1,6 @@
 var Twitter = require('twitter');
+var PushBullet = require('pushbullet');
+
 var config = require('./config.json');
 
 var client = new Twitter({
@@ -7,6 +9,8 @@ var client = new Twitter({
   access_token_key: config.twitter_access_token_key,
   access_token_secret: config.twitter_access_token_secret
 });
+
+var pusher = new PushBullet(config.pushbullet_key);
 
 
 var searchTerm = "javascript";
@@ -25,9 +29,19 @@ function startStream (conn) {
 		});
 
 		stream.on('error', function(error) {
+      sendNotification();
 			throw error;
   		});
 	});
+}
+
+function sendNotification () {
+  pusher.note(config.pushbullet_device_id_iphone, config.pushbullet_msg_title, config.pushbullet_msg_title, function(error, response) {
+    // response is the JSON response from the API
+    console.log("pusher.note: " + response);
+});
+
+
 }
 
 startStream();
